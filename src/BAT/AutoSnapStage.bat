846be21dc1d0dd64171eff833e6ab0af4c371650
ryotksm <39453788+ryotksm@users.noreply.github.com>
2018-09-18 02:53:38 +0900

    自動撮影バッチ（作成中）を追加

---
+++ src/BAT/AutoSnapStage.bat
@@ -0,0 +1,31 @@
+mode COM3: BAUD=57600 DATA=8 STOP=1 dtr=off rts=off PARITY=N
+'‚PT‰ñ‚·
+echo ";RotateStage(15,5);" >> COM3
+echo ";RotateStage(15,5);" >> COM3
+echo ";RotateStage(15,5);" >> COM3
+echo ";RotateStage(15,5);" >> COM3
+echo ";RotateStage(15,5);" >> COM3
+echo ";RotateStage(15,5);" >> COM3
+echo ";RotateStage(15,5);" >> COM3
+echo ";RotateStage(15,5);" >> COM3
+echo ";RotateStage(15,5);" >> COM3
+echo ";RotateStage(15,5);" >> COM3
+echo ";RotateStage(15,5);" >> COM3
+echo ";RotateStage(15,5);" >> COM3
+echo ";RotateStage(15,5);" >> COM3
+echo ";RotateStage(15,5);" >> COM3
+echo ";RotateStage(15,5);" >> COM3
+echo ";RotateStage(15,5);" >> COM3
+echo ";RotateStage(15,5);" >> COM3
+echo ";RotateStage(15,5);" >> COM3
+echo ";RotateStage(15,5);" >> COM3
+echo ";RotateStage(15,5);" >> COM3
+echo ";RotateStage(15,5);" >> COM3
+echo ";RotateStage(15,5);" >> COM3
+echo ";RotateStage(15,5);" >> COM3
+echo ";RotateStage(15,5);" >> COM3
+echo ";RotateStage(15,5);" >> COM3
+
+'­‚µ‰º‚°‚é
+echo " ;MoveCamera(500,200); " >> COM3
+

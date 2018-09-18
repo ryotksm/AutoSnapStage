244070410a9d0c70f432d35b1fe28d1bd04a9231
ryotksm <39453788+ryotksm@users.noreply.github.com>
2018-09-19 02:01:22 +0900

    OpenCVとC++のかわりにffmpegとバッチファイルで実装。

--- src/BAT/AutoSnapStage.bat
+++ src/BAT/AutoSnapStage.bat
@@ -1,31 +1,17 @@
+cd /d %~dp0
 mode COM3: BAUD=57600 DATA=8 STOP=1 dtr=off rts=off PARITY=N
-'‚PT‰ñ‚·
-echo ";RotateStage(15,5);" >> COM3
-echo ";RotateStage(15,5);" >> COM3
-echo ";RotateStage(15,5);" >> COM3
-echo ";RotateStage(15,5);" >> COM3
-echo ";RotateStage(15,5);" >> COM3
-echo ";RotateStage(15,5);" >> COM3
-echo ";RotateStage(15,5);" >> COM3
-echo ";RotateStage(15,5);" >> COM3
-echo ";RotateStage(15,5);" >> COM3
-echo ";RotateStage(15,5);" >> COM3
-echo ";RotateStage(15,5);" >> COM3
-echo ";RotateStage(15,5);" >> COM3
-echo ";RotateStage(15,5);" >> COM3
-echo ";RotateStage(15,5);" >> COM3
-echo ";RotateStage(15,5);" >> COM3
-echo ";RotateStage(15,5);" >> COM3
-echo ";RotateStage(15,5);" >> COM3
-echo ";RotateStage(15,5);" >> COM3
-echo ";RotateStage(15,5);" >> COM3
-echo ";RotateStage(15,5);" >> COM3
-echo ";RotateStage(15,5);" >> COM3
-echo ";RotateStage(15,5);" >> COM3
-echo ";RotateStage(15,5);" >> COM3
-echo ";RotateStage(15,5);" >> COM3
-echo ";RotateStage(15,5);" >> COM3
 
-'­‚µ‰º‚°‚é
-echo " ;MoveCamera(500,200); " >> COM3
+for /l %%i in (1,1,16) do (
+  for /l %%j in (1,1,24) do (
+    echo ";RotateStage(15,5);" >> COM3
+    ffmpeg -y -f dshow -video_size 1280x720 -i video="HD 720P Webcam" -s 1280x720 -ss 5 -t 1 -r 1 -f image2 -vcodec png %%i-%%j.png
+  )
+  echo " ;MoveCamera(500,200); " >> COM3
+)
+echo " ;MoveCamera(-8000,100); " >> COM3
 
+pause
+echo " ;MoveCamera(-100,50); " >> COM3
+echo " ;MoveCamera(50,50); " >> COM3
+
+echo " ;MoveCamera(8000,100); " >> COM3
